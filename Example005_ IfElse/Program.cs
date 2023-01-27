2dd95f3e920d610357e05449a9a924add5f2ada9
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-01-27 21:20:49 +0700

    add ex005

---
+++ Example005_ IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользоателя:");
+string username = Console.ReadLine();
+ 
+ if(username.ToLower() =="тоня")
+ {
+    Console.WriteLine("Ура,это же ТОНЯ!");
+ }
+ else
+ {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+ }
